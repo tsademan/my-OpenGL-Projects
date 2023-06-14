0f966e7b5a668e1ef875d8c2c6ecf101f44a2955
Your Name <you@example.com>
2023-06-15 00:09:52 +0300

    commit -3

--- BrokenLineWzText/main.cpp
+++ BrokenLineWzText/main.cpp
@@ -1,24 +1,17 @@
 #include <windows.h>
 #include <GL/glut.h>
 
-
-
 void init() {
-
   glClearColor(0, 1, 1, 1);
   glMatrixMode(GL_PROJECTION);
   glOrtho(-50, 100, -50, 100, -50, 100);
-
 }
 void display() {
-
   glClear(GL_COLOR_BUFFER_BIT);
-
   glEnable(GL_LINE_STIPPLE);//enabling gl stipple
   glLineStipple(15, 0x1111);//factor and pattern the value of factor is integar
   //the value of pattern is hezdecimal
   glLineWidth(1000);
-
 
   glColor3f(0, 0, 0);
   glBegin(GL_LINES);
@@ -36,20 +29,6 @@ void display() {
   glBegin(GL_LINES);
   glVertex2i(-20, 30);
   glVertex2i(50, 30);
-
-  glEnd();
-
-  //triangle
-
-  glLineWidth(10);
-  glBegin(GL_TRIANGLES);//color full triangle
-  glColor3f(1, 0, 0);
-  glVertex2i(-20, 0);
-  glColor3f(0, 0, 1);
-  glVertex2i(20, 50);//the red line
-  glColor3f(0, 1, 0);
-  glVertex2i(20, 0);
-
   glEnd();
 
   glFlush();
---
+++ ToWriteString/main.cpp
@@ -0,0 +1,35 @@
+#include<windows.h>
+#include<gl/glut.h>
+
+void init()
+{
+    glClearColor(1,1,1,1);
+    glMatrixMode(GL_PROJECTION);
+    glOrtho(-20,20,-20,20,-20,20);
+}
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0,0,0);
+    glRasterPos2i(2,10);
+    const char*mystring ="Hello,World";
+    for(int i=0;i<strlen(mystring);++i)
+    {
+    glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,mystring[i]);
+    }
+ glFlush();
+}
+
+int main(int arg,char** argv)
+{
+    glutInit(&arg,argv);
+    glutInitDisplayMode(GLUT_RGB|GLUT_SINGLE);
+    glutInitWindowPosition(700,600);
+    glutInitWindowSize(500,400);
+    glutCreateWindow("my string");
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+
+}
